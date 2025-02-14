02810aa56d207021d58e29c2d2b29b6861087b04
Komal-Rauniyar <komalrauniyar74@gmail.com>
2025-02-14 23:00:42 +0530

    Updated database configuration for deployment

--- config/database.ts
+++ config/database.ts
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'dpg-cunjmhq3esus73cj6r90-a.oregon-postgres.render.com'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'chatlist'),  // Ensure this database exists
       user: env('DATABASE_USERNAME', 'komal'),    // Change if needed
